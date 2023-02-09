083aa69c354b08f890a6b0e01419094d70459759
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-02-08 20:03:08 -0500

    Initial Commit

---
+++ PoolCalculator/PoolCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolCalculatorApp.swift
+//  PoolCalculator
+//
+//  Created by Emanuel Gross von Trockau on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct PoolCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
