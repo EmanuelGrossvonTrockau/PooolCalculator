f61f8cfb996b8cd5a0740ae29c2dee71d6d87995
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-02-08 20:08:12 -0500

    Created a NavigationView

--- PoolCalculator/ContentView.swift
+++ PoolCalculator/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("T")
         }
-        .padding()
-    }
+        
+        .navigationTitle("Pool Calculator")
+            }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
