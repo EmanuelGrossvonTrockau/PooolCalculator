3d95f426001881a33ce0c3307996bdf496d881f1
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-02-09 21:41:41 -0500

    Added a tabView

--- PoolCalculator/PoolCalculatorApp.swift
+++ PoolCalculator/PoolCalculatorApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct PoolCalculatorApp: App {
+    @State var history: [Result] = []
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                ContentView( history: $history)
+                    .tabItem {
+                        Image(systemName: "rectangle.split.2x2.fill")
+                        Text("Calculate")
+                    }
+                 HistoryView(history: $history)
+                    .tabItem {
+                        Image(systemName: "Clock.fill")
+                        Text("History")
+                    }
+                
+                
+                
+            }
+            
         }
     }
 }
